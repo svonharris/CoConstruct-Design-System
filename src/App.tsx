d0b6b6e83f8bad167baa6fb9f2a1b600824bd191
Sheonna Harris <svonharris@gmail.com>
2026-01-16 15:01:48 -0500

    Added url Prop to PricingCard.

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,21 @@ function App() {
           price="$99"
           promo="$399 after 2 months"
           description="For teams looking to achieve more efficient job sites and simpler project planning. You may just be getting started with software or want to focus on project management and communication."
+          href="https://www.coconstruct.com/Signups/A302M"
         />
         <PricingCard
           plan="Advanced"
           price="$399"
           promo="$699 after 2 months"
           description="For builders who need to create estimates with confidence and are looking for fuller financial control. All tools from Essential are included."
+          href="https://www.coconstruct.com/Signups/B302M"
         />
         <PricingCard
           plan="Complete"
           price="$899"
           promo="$1,299 after 2 months"
           description="For those ready to centralize all complex processes – including selections and RFIs – into one simple platform. You’ll have the tools from Essential and Advanced, too."
+          href="https://www.coconstruct.com/Signups/C302M"
         />
       </SectionFixed3Col>
     </>
