3177f772e6c901fbab663ed02f4f0fcf08da127b
Sheonna Harris <svonharris@gmail.com>
2026-01-22 18:58:11 -0500

    Changed component name form Input to FormInput.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Button from "./components/Button/Button";
 import Toggle from "./components/ToggleSwitch/ToggleSwitch";
 import Tabs from "./components/Tabs/Tabs";
 import HeroSection from "./components/HeroSection/HeroSection";
-import Form from "./components/Form/Form";
+import Form from "./components/Form/FormInput";
 
 function App() {
   return (
