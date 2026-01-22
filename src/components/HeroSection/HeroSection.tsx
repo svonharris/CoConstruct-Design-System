1637ca5c5adf80af0b95e8aae38d207c191b72ad
Sheonna Harris <svonharris@gmail.com>
2026-01-21 20:05:52 -0500

    Added boolean Prop to add background image for component.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -3,14 +3,22 @@ import style from "./HeroSection.module.css";
 type HeroSectionProps = {
   heading: string;
   subheading: string;
+  hasBackground?: boolean;
 };
 
-const HeroSection = ({ heading, subheading }: HeroSectionProps) => {
+const HeroSection = ({
+  heading,
+  subheading,
+  hasBackground = false,
+}: HeroSectionProps) => {
   return (
-    <>
+    <div
+      className={hasBackground ? style.containerBackground : style.container}
+    >
       <h1 className={style.title}>{heading}</h1>
       <p className={style.subtitle}>{subheading}</p>
-    </>
+    </div>
   );
 };
+
 export default HeroSection;
