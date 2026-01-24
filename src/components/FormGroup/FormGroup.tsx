58d6e8a3381a2aaa8ee633903a60647d3eefb250
Sheonna Harris <svonharris@gmail.com>
2026-01-23 21:42:58 -0500

    Added Props: id, label, required to component.

--- src/components/FormGroup/FormGroup.tsx
+++ src/components/FormGroup/FormGroup.tsx
@@ -2,10 +2,27 @@ import style from "./FormGroup.module.css";
 
 type FormGroupProps = {
   children: React.ReactNode;
+  id: string;
+  label: string;
+  required?: boolean;
 };
 
-const FormGroup = ({ children }: FormGroupProps) => {
-  return <div className={style.formGroup}>{children}</div>;
+const FormGroup = ({
+  children,
+  id,
+  label,
+  required = false,
+}: FormGroupProps) => {
+  return (
+    <div className={style.formGroup}>
+      <label htmlFor={id} className={style.formLabel}>
+        {label}
+        {required && <span className={style.formRequired}>*</span>}
+      </label>
+
+      {children}
+    </div>
+  );
 };
 
 export default FormGroup;
