9c09c24642229539b02b51bb92b603b026cfbcef
Sheonna Harris <svonharris@gmail.com>
2026-01-24 23:29:02 -0500

    Removed button with a Modal and Form. Removed Link component. Removed navLinks array and mapping logic. Added boolean prop to hide/show hamburger menu. Added children to props.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,22 +1,13 @@
 import { useState } from "react";
 import style from "./NavBar.module.css";
-import Modal from "../Modal/Modal";
-import Form from "../Form/Form";
 import Logo from "../Logo/Logo";
-import Link from "../Link/Link";
 
-type NavLinkOptionProps = {
-  href: string;
-  label: string;
-  ariaLabel: string;
-  title?: string;
+type NavBarProps = {
+  children?: React.ReactNode;
+  showHamburger?: boolean;
 };
 
-type NavLinkProps = {
-  navLinks: NavLinkOptionProps[];
-};
-
-const NavBar = ({ navLinks }: NavLinkProps) => {
+const NavBar = ({ children, showHamburger = true }: NavBarProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
@@ -29,44 +20,24 @@ const NavBar = ({ navLinks }: NavLinkProps) => {
         <Logo />
 
         {/* Hamburger Menu */}
-        <button
-          className={`${style.hamburger} ${isMenuOpen ? style.active : ""}`}
-          onClick={toggleMenu}
-          aria-label="View the menu"
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
+        {showHamburger && (
+          <button
+            className={`${style.hamburger} ${isMenuOpen ? style.active : ""}`}
+            onClick={toggleMenu}
+            aria-label="View the menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        )}
 
-        {/* Navigation Links */}
+        {/* Navigation Content */}
         <div
           className={`${style.navMenu} ${isMenuOpen ? style.active : ""}`}
           onClick={closeMenu}
         >
-          <div className={style.navLinks}>
-            {navLinks.map((option) => (
-              <Link
-                href={option.href}
-                label={option.label}
-                ariaLabel={option.ariaLabel}
-                title={option.title ? option.title : option.ariaLabel}
-                variant="navigation"
-              />
-            ))}
-          </div>
-
-          {/* Button */}
-          <Modal
-            buttonLabel="Schedule a Demo"
-            ariaLabel="Open the schedule a demo form"
-            variant="secondary"
-            title="Open the schedule a demo form"
-          >
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <Form />
-            </div>
-          </Modal>
+          {children}
         </div>
       </div>
     </nav>
