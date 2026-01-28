39e1b28ea4292aebc9a8d2be8d9931d2c761b102
Sheonna Harris <svonharris@gmail.com>
2026-01-27 19:22:41 -0500

    Created a basic Footer component.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import style from "./Footer.module.css";
+import Logo from "../Logo/Logo";
+
+type FooterProps = {
+  children?: React.ReactNode;
+};
+
+const Footer = ({ children }: FooterProps) => {
+  return (
+    <>
+      <footer className={style.container}>
+        <div className={style.heading}>
+          <Logo variant="compact" />
+        </div>
+        <hr className={style.divider}></hr>
+        <div className={style.content}>{children}</div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
