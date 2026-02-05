9ede197544c37b8315d9080b8ab89749936d1beb
Sheonna Harris <svonharris@gmail.com>
2026-02-05 14:59:17 -0500

    Added index.css import. Added autodocs for Storybook.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react-vite'
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
@@ -16,6 +17,7 @@ const preview: Preview = {
       test: 'todo'
     }
   },
+  tags: ['autodocs'],
 };
 
 export default preview;
