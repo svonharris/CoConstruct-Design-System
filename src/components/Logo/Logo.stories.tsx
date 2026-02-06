da06ed6ca03bb068aeb871ab83da434d604f8f77
Sheonna Harris <svonharris@gmail.com>
2026-02-05 23:17:22 -0500

    Created Logo story.

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Logo from "./Logo";
+
+const meta = {
+  title: "Components/Logo",
+  component: Logo,
+  argTypes: {
+    variant: {
+      control: false, //renders prop documentation without a control
+    },
+    href: {
+      control: false,
+    },
+    altText: {
+      control: false,
+    },
+    ariaLabel: {
+      control: false,
+    },
+    title: {
+      table: { disable: true }, //removes prop control
+    },
+    width: {
+      control: "text",
+    },
+  },
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+type Story = StoryObj<typeof Logo>;
+
+export const Default: Story = {
+  args: {
+    width: "185px",
+  },
+};
+
+export const Compact: Story = {
+  args: {
+    width: "55px",
+    variant: "compact",
+  },
+};
