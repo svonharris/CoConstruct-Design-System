1a650b3be9218abff0cdd0bdc0f5d7ad783962e9
Sheonna Harris <svonharris@gmail.com>
2026-02-05 21:35:46 -0500

    Created a HeroSection story.

---
+++ src/components/HeroSection/HeroSection.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import HeroSection from "./HeroSection";
+
+const meta = {
+  title: "Components/HeroSection",
+  component: HeroSection,
+  argTypes: {
+    hasBackground: {
+      control: false, //renders prop documentation without a control
+    },
+    heading: {
+      control: "text",
+    },
+    subheading: {
+      control: "text",
+    },
+  },
+} satisfies Meta<typeof HeroSection>;
+
+export default meta;
+type Story = StoryObj<typeof HeroSection>;
+
+/** Hero Section without a background. */
+export const Default: Story = {
+  args: {
+    heading: "Section heading here",
+    subheading: "Write some subheading text here.",
+  },
+};
+
+/** Hero Section with a background design. */
+export const WithBackground: Story = {
+  args: {
+    ...Default.args,
+    hasBackground: true,
+  },
+};
