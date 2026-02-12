124543dc70828d2fb0210213e4907dac587c6b22
Sheonna Harris <svonharris@gmail.com>
2026-02-12 18:41:09 -0500

    Moved args from stories to global args.

--- src/components/HeroSection/HeroSection.stories.tsx
+++ src/components/HeroSection/HeroSection.stories.tsx
@@ -4,6 +4,10 @@ import HeroSection from "./HeroSection";
 const meta = {
   title: "Components/Hero Section",
   component: HeroSection,
+  args: {
+    heading: "Section heading here",
+    subheading: "Write some subheading text here.",
+  },
   argTypes: {
     hasBackground: {
       control: false, //renders prop documentation without a control
@@ -21,17 +25,11 @@ export default meta;
 type Story = StoryObj<typeof HeroSection>;
 
 /** Hero Section without a background. */
-export const Default: Story = {
-  args: {
-    heading: "Section heading here",
-    subheading: "Write some subheading text here.",
-  },
-};
+export const Default: Story = {};
 
 /** Hero Section with a background design. */
 export const WithBackground: Story = {
   args: {
-    ...Default.args,
     hasBackground: true,
   },
 };
