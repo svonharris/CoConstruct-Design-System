760405899df422e246665f5a9adfb364fd682e86
Sheonna Harris <svonharris@gmail.com>
2026-02-12 18:41:56 -0500

    Moved args from stories to global args. Added argTypes for children.

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -4,14 +4,18 @@ import Footer from "./Footer";
 const meta = {
   title: "Components/Footer",
   component: Footer,
+  args: {
+    children: "© 2024 CoConstruct. All rights reserved.",
+  },
+  argTypes: {
+    children: {
+      control: "text",
+    },
+  },
 } satisfies Meta<typeof Footer>;
 
 export default meta;
 type Story = StoryObj<typeof Footer>;
 
 /** Default Footer design and code. */
-export const Default: Story = {
-  args: {
-    children: "© 2024 CoConstruct. All rights reserved.",
-  },
-};
+export const Default: Story = {};
