9c11b71cdfa1f61adda2a2110e3de73f13d437b0
Sheonna Harris <svonharris@gmail.com>
2026-02-18 12:15:39 -0500

    Moved story args & argTypes to meta object.

--- src/components/Button/Button.playground.stories.tsx
+++ src/components/Button/Button.playground.stories.tsx
@@ -4,15 +4,23 @@ import Button from "./Button";
 const meta = {
   title: "Internal/Playground/Button", // hide in internal section
   component: Button,
+  args: {
+    children: "Button",
+    variant: "primary",
+    disabled: false,
+  },
   argTypes: {
     children: { description: "The content to be displayed inside the button." },
     variant: {
+      control: "radio",
       description: "Determines the visual style of the button.",
     },
     disabled: {
+      control: "boolean",
       description: "Disables the button and prevents user interaction.",
     },
     title: {
+      control: false,
       description:
         "Additional information about the button, often used for accessibility and tooltip.",
     },
@@ -28,19 +36,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const PlaygroundDocs: Story = {
-  args: {
-    children: "Button",
-    variant: "primary",
-    disabled: false,
-  },
-  argTypes: {
-    variant: {
-      control: "radio",
-    },
-    disabled: {
-      control: "boolean",
-    },
-    title: { control: false },
-  },
-};
+export const PlaygroundDocs: Story = {};
