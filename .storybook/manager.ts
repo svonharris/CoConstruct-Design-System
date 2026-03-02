2cca704eac73b171c5f06dec6bfdd38075d44a73
Sheonna Harris <svonharris@gmail.com>
2026-03-01 19:43:17 -0500

    Updated URL for brand logo.

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -6,6 +6,6 @@ addons.setConfig({
     base: 'light',
     brandTitle: '',
     brandUrl: '/',
-    brandImage: './CoConstruct_Logo-square.png',
+    brandImage: '/CoConstruct-Design-System/storybook-static/CoConstruct_Logo-square.png',
   }),
 });
