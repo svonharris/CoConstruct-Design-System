079dd78fc5ec4c1093a2507306e85f0fe75996e5
Sheonna Harris <svonharris@gmail.com>
2026-03-25 03:18:27 -0400

    Added a title.

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,7 +4,7 @@ import { create } from 'storybook/theming';
 addons.setConfig({
   theme: create({
     base: 'light',
-    brandTitle: '',
+    brandTitle: 'CoConstruct Design System',
     brandUrl: './',
     brandImage: './CoConstruct_Logo-square.png',
   }),
